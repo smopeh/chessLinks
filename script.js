44dac7659d4ad4751b1c7e32afbca584871a61da
smopeh <lvdveen@icloud.com>
2025-02-16 17:09:55 +0100

    Update script.js
    add the Ipa functions to script2

--- script.js
+++ script.js
@@ -1,64 +1,40 @@
-// export data after fetching data
-// store api key in .env
+import { runFunctions } from "./script2.js";
 
-//make link adjustable with user input
-//add loading animatio
+let toggleLoading = document.getElementsByClassName("centerBlock")[0];
+let toggleLinks = document.getElementsByClassName("centerBlock")[1];
+const dropdown = document.getElementById("dropDown");
 
-const apiKey = "";
-const baseUrl = "https://www.googleapis.com/youtube/v3";
-const requestPath = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=GMHikaru&type=channel&key=${apiKey}`;
-let targetChannel = "";
-const videoMemory = [];
+toggleLoading.style.display = "none";
+toggleLinks.style.display = "none";
 
-async function youtube() {
-  try {
-    const data = await fetch(requestPath);
-    if (!data.ok) {
-      throw new Error(data.status + " Explenation: " + data.statusText);
+async function loading(name) {
+  toggleLoading.style.display = "flex";
+  toggleLinks.style.display = "none";
+  
+    try {
+      const videos = await runFunctions(name);
+      console.log(videos[0]);
+      for (let i = 0; i < 5; i++) {
+        document.getElementById(`element${i+1}`).href = `https://www.youtube.com/watch?v=${videos[i].snippet.resourceId.videoId}`;
+      }
+      document.getElementsByClassName("centerBlock")[1].style.display = "flex";
+      document.getElementsByClassName("centerBlock")[0].style.display = "none";
+    } catch(error) {
+      console.log(error);
     }
-    const jsonData = await data.json();
-    return jsonData.items[0].id.channelId;
-  } catch(error) {
-    console.log(error);
-  }
-};
-
-async function findChannel(targetChannel) {
-  try {
-    const response = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=${targetChannel}&key=${apiKey}`);
-    if (response.ok) {
-      const jsonResponse = await response.json();
-      return jsonResponse.items[0].contentDetails.relatedPlaylists.uploads;
-    }
-  } catch(error) {
-    console.log(error);
-  }
-};
-
-async function getVideos(playlistId) {
-  try {
-    const response = await fetch(`${baseUrl}/playlistItems?part=snippet&maxResults=10&playlistId=${playlistId}&key=${apiKey}`);
-    const jsonResponse = await response.json();
-    return jsonResponse;
-  } catch(error) {
-    console.log(error);
-  }
 }
 
-async function runFunctions() {
-  const response = await youtube();
-  const data = await findChannel(response);
-  const videoJson = await getVideos(data);
-  return videoJson.items;
-};
-
-runFunctions()
-  .then(video => {
-    for (let i = 0; i < 5; i++) {
-      document.getElementById(`link${i+1}`).href = `https://www.youtube.com/watch?v=${video[i].snippet.resourceId.videoId}`;
-    }
-  })
-  .catch (data => {
-    console.log(data)
-  });
-
+dropdown.addEventListener("change", function() {
+  switch (dropdown.value) {
+    case "":
+      toggleLinks.style.display = "none";
+      toggleLoading.style.display = "none";
+      break;
+    case "GMHikaru":
+    case "TruckerTim":
+      loading(dropdown.value);
+      break;
+    default:
+      break;
+  }
+});
